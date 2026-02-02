2c97480d94c322b45f3fd5a39641ec399809fa89
Evan Fekete <88007611+Evan-Fekete@users.noreply.github.com>
2026-02-02 09:37:24 -0500

    Added Project for Lab 2

---
+++ Lab2/src/lab2part1/Lab2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2part1;
+
+/**
+ *
+ * @author evan_
+ */
+public class Lab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
