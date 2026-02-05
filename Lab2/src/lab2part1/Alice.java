959c99abffd1918ff33a5d36809118653d45502c
Evan Fekete <evan_fekete@outlook.com>
2026-02-05 00:43:41 -0500

    Project 1 code is generally working

--- Lab2/src/lab2part1/Alice.java
+++ Lab2/src/lab2part1/Alice.java
@@ -6,30 +6,102 @@ package lab2part1;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.Base64;
+import java.util.Random;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 
 /**
  *
  * @author evan_
  */
 public class Alice {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
         if (args.length != 2) {
             System.err.println("Error: two arguments needed, host and port number");
             System.exit(1);
         }
         
+        // Key name must have at least 8 characters or 64 bits
+        String key = "GreatKeyName", id, bobInput, message;
+        String[] firstBobArr, secondBobArr;
+        Random r = new Random();
         String hostName = args[0];
         int portNumber = Integer.parseInt(args[1]);
         
-        try (Socket bobSock = new Socket(hostName, portNumber);
-            BufferedReader in = new BufferedReader(new InputStreamReader(bobSock.getInputStream()));
-            PrintWriter out = new PrintWriter(bobSock.getOutputStream(), true);
-             Scanner scanner = new Scanner(System.in)) {
-                
+        try (Socket aliceSock = new Socket(hostName, portNumber);
+                BufferedReader in = new BufferedReader(new InputStreamReader(aliceSock.getInputStream()));
+                PrintWriter out = new PrintWriter(aliceSock.getOutputStream(), true)) {    
+            
+            // Concatenate nonce to id
+            id = "Alice";
+            int nonce = r.nextInt(1000);
+            message = id + "," + nonce;
+            
+            // Send first message (IDa || Na)
+            System.out.println("Sending: " + message);
+            out.println(message);
+            
+            // Wait for Bob to send response (Nb||E(KAB,[IDb||NA]))
+            bobInput = in.readLine();
+            System.out.println("Received: " + bobInput);
+            
+            // Split Bob's response at commas
+            // First half of response is Nb
+            // Second half of response is E(Kab, [IDb || Na])
+            firstBobArr = bobInput.split(",");
+            
+            // Generate a DES Key
+            DESKeySpec desKeySpec = new DESKeySpec(key.getBytes());
+            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey secretKey = secretKeyFactory.generateSecret(desKeySpec);
+            
+            // Create a cipher instance
+            Cipher cipher = Cipher.getInstance("DES");
+            
+            // Initialize the cipher for decryption
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            
+            // Decrypt the ciphertext
+            byte[] decodedData = cipher.doFinal(Base64.getDecoder().decode(firstBobArr[1]));
+            String decodedDataString = new String(decodedData);
+            System.out.println("The decoded data is: " + decodedDataString);
+            
+            // Split Bob's encrypted message at commas
+            // First half of response is IDb
+            // Second half of response is Na
+            secondBobArr = decodedDataString.split(",");
+            
+            // Check if Nb received from Bob is correct
+            if (secondBobArr[1].equals(String.valueOf(nonce))) {
+                System.out.println("Nonce has been verified. ");
+            }
+            else {
+                System.out.println("Mismatched Nonce Exiting...");
+                System.exit(0);
+            }
+            
+            //
+            message = id + "," + firstBobArr[0];
+            
+            // Initialize cipher for encryption
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+                    
+            // Encrypt plaintext
+            byte[] cipherTextArray = cipher.doFinal(message.getBytes());
+            String cipherTextString = Base64.getEncoder().encodeToString(cipherTextArray);
+            
+            System.out.println("Sending: " + message);
+            
+            // Send final message to Bob (E(Kab,[IDa|Nb]))
+            out.print(cipherTextString);
+            
+            System.out.println("Alice disconnected.");
         } catch(IOException e) {
-                System.err.println("Client error: " + e.getMessage());
+            System.err.println("Client error: " + e.getMessage());
         }
     }
 }
--- Lab2/src/lab2part1/Bob.java
+++ Lab2/src/lab2part1/Bob.java
@@ -10,62 +10,98 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.Random;
+import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.DESKeySpec;
 import javax.crypto.SecretKeyFactory;
+import java.util.Base64;
 
 /**
  *
  * @author evan_
  */
 public class Bob {
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        int portNumber = Integer.parseInt(args[0]);
+    public static void main(String[] args) throws Exception {
         
         if (args.length != 1) {
             System.err.println("Error: one argument needed, port number");
             System.exit(1);
         }
         
-        System.out.println("SiriServer listening on port number " + portNumber);
+        // Key name must have at least 8 characters or 64 bits
+        String secretKey = "GreatKeyName", message, id;
+        String[] firstAliceArr, secondAliceArr;
+        Random r = new Random();
+        int portNumber = Integer.parseInt(args[0]);
+        
+        System.out.println("Bob listening on port number " + portNumber);
         try (ServerSocket bobSock = new ServerSocket(portNumber)) {
-            while (true) {
-                try (Socket aliceSock = bobSock.accept();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(aliceSock.getInputStream()));
-                     PrintWriter out = new PrintWriter(
-                             aliceSock.getOutputStream(), true)) {
+            String aliceInput;
 
-                    System.out.println("Alice connected: " + aliceSock.getRemoteSocketAddress());
+            try (Socket aliceSock = bobSock.accept();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(aliceSock.getInputStream()));
+                    PrintWriter out = new PrintWriter(aliceSock.getOutputStream(), true)) {
 
-                    // Enter message for DES to encrypt
+                System.out.println("Alice connected: " + aliceSock.getRemoteSocketAddress());
 
-                    // Generate a DES Key
-                    
-                    // Create a cipher instance to be used for encryption and decryption
-                    
-                    // Initialize cipher for encryption
-                    
-                    // Encrypt the plaintext
-                    
-                    // Encode the encrypted bytes for readability
-                    
-                    // Initialize the cipher for decryption
-                    
-                    // Decrypt the ciphertext
-                    
-                    
-                    System.out.println("Client disconnected.");
-                } catch (IOException e) {
-                    System.err.println("Handler error: " + e.getMessage());
+                // Once connection is established wait for Alice to send first message
+                aliceInput = in.readLine();
+                System.out.println("Alice sent: " + aliceInput);
+
+                //  Split Alice's message at commas to seperate variables
+                firstAliceArr = aliceInput.split(",");
+
+                // Create message for DES to encrypt (IDb || Na)
+                id = "Bob";
+                int nonce = r.nextInt(1000);
+                message = id + "," + firstAliceArr[1];
+
+                System.out.println("Encrypting: " + message);
+
+                // Generate a DES Key
+                DESKeySpec desKeySpec = new DESKeySpec(secretKey.getBytes());
+                SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DES");
+                SecretKey secretKey1 = secretKeyFactory.generateSecret(desKeySpec);
+
+                // Create a cipher instance to be used for encryption and decryption
+                Cipher cipher = Cipher.getInstance("DES");
+
+                // Initialize cipher for encryption
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey1);
+
+                // Encrypt plaintext
+                byte[] cipherTextArray = cipher.doFinal(message.getBytes());
+
+                // Encode encrypted bits to properly display encrypted plaintext
+                String cipherTextString = Base64.getEncoder().encodeToString(cipherTextArray);
+                System.out.println("Cipher text of Bob Message is " + cipherTextString);
+
+                // Send message with Nonce to Alice (Nb||E(KAB,[IDb||NA]))
+                message = nonce + "," + cipherTextString;
+                out.println(message);
+                aliceInput = in.readLine();
+
+                // Initialize the cipher for decryption
+                cipher.init(Cipher.DECRYPT_MODE, secretKey1);
+                byte[] decodedData = cipher.doFinal(Base64.getDecoder().decode(aliceInput));
+                String decodedDataString = new String(decodedData);
+
+                // Decrypt the ciphertext
+                System.out.println("The decoded data is: " + decodedDataString);
+                secondAliceArr = decodedDataString.split(",");
+
+                if (secondAliceArr[1].trim().equals(String.valueOf(nonce))) {
+                    System.out.println("Nonce has been verified. ");
                 }
+                else {
+                    System.out.println("Mismatched Nonce Exiting...");
+                    System.exit(0);
+                }
+
+                System.out.println("Bob disconnected.");
+            } catch (IOException e) {
+                System.err.println("Handler error: " + e.getMessage());
             }
         } catch (IOException e) {
             System.err.println("Server error: " + e.getMessage());
